c2521eddd1e3bf2499cef03a92d9a38fc5990d51
muquu <muquu@skytrum.com>
2020-07-11 08:20:11 +0900

    setup.sh 編集

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ do
         echo $HOME/$file exist. backup file $HOME/$file~ is created.
     fi
     ln -sb $HOME/dotfiles/$file $HOME/$file
+    mkdir -p $HOME/.config
+    ln -s $HOME/dotfiles/fish $HOME/.config/
 done
 
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
